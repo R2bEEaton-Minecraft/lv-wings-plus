e9e298f4ec7c0bb5058963235df275d6efa07e81
Ryan Eaton <ryaneaton@spea.cc>
2026-02-10 10:07:13 -0500

    Fix crashing issue with modpack

--- src/main/java/com/toni/wings/client/model/ModelWingsAvian.java
+++ src/main/java/com/toni/wings/client/model/ModelWingsAvian.java
@@ -208,7 +208,7 @@ public final class ModelWingsAvian extends ModelWings<AnimatorAvian> {
         private static List<ModelPart.Cube> getCubes(ModelPart part) {
                 try {
                         List<ModelPart.Cube> cubes = (List<ModelPart.Cube>) CUBES_FIELD.get(part);
-                        if (cubes instanceof ImmutableList) {
+                        if (!(cubes instanceof ArrayList)) {
                                 cubes = new ArrayList<>(cubes);
                                 CUBES_FIELD.set(part, cubes);
                         }
